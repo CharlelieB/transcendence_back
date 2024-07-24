523ff3b8decc4524dd6cb92fc98a1582b20b2536
Charlelie BESSONNET <cbessonn@made-f0Cr4s2.clusters.42paris.fr>
2024-07-24 18:05:45 +0200

    generate new secret key and use env variable secret key instead in settings.py

--- transcendence/settings.py
+++ transcendence/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-i)k(sxl3=z9i9__i*m@z*8jj=0w_qbw6t1wm5-j*&w$#+c_w2o'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
