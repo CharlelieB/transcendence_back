7a4f4af26e9ea03b5c126394b576dd24f1960b25
nassim <110173111+PlausiblyBoy@users.noreply.github.com>
2024-08-19 17:19:19 +0200

    test 1

--- transcendence/settings.py
+++ transcendence/settings.py
@@ -191,7 +191,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [BASE_DIR / "frontend/static"]
+STATICFILES_DIRS = [BASE_DIR / "frontend/"]
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
 MEDIA_URL = '/media/'
