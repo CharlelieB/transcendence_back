46180b5661b6e4268f7ed61af6106f2fabb3a601
HelloThere-ImMat <dorr.mathias@gmail.com>
2024-08-23 17:33:01 +0200

    finished login

--- frontend/static/actions.js
+++ frontend/static/actions.js
@@ -24,13 +24,16 @@ function getCookie(name) {
 function ReplaceElement(elementToHideId, elementToShowId)
 {
 	var elementToHide = document.getElementById(elementToHideId);
-	var elementToShow = document.getElementById(elementToShowId);
 
 	elementToHide.classList.add("d-none");
 	elementToHide.classList.remove("d-flex");
 
-	elementToShow.classList.remove("d-none");
-	elementToShow.classList.add("d-flex");
+	if (elementToShowId !== "null")
+	{
+		var elementToShow = document.getElementById(elementToShowId);
+		elementToShow.classList.remove("d-none");
+		elementToShow.classList.add("d-flex");
+	}
 }
 
 /////    FUNCTIONS TO DISPLAY HTML ELEMENTS /////
@@ -45,10 +48,16 @@ document.addEventListener('DOMContentLoaded', function() {
 			ReplaceElement("buttonsContainer", "playerConnection");
 			ReplaceElement("loginBackButton", "null");
 		}
-		//else {
-		//	JSONformat  = response.json();
-		//	document.getElementById("testContainer").innerText(JSONformat);
-		//}
+		return response.json()
+	})
+	.then(data => {
+		if (data.access) {
+			window.accessToken = data.access;
+			console.log(window.accessToken);
+			console.log('Access token saved');
+		} else {
+			console.error("access token not saved");
+		}
 	})
  });
 
@@ -135,9 +144,9 @@ document.addEventListener('DOMContentLoaded', function () {
 
 /////        API CALLS      //////
 
-const AccessKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzI0MTYxNjgxLCJpYXQiOjE3MjQxNTU2ODEsImp0aSI6ImM3NjQ3OWE3ODY4MDQyOGRiMmM5ZjhmMzRhNjA3YTE4IiwidXNlcl9pZCI6Mn0.PtLbOUmuE5YK4Bp3htv6fzao9_fPUTAQMLIYszkuYiU"
+const AccessKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzI0NDExNjA4LCJpYXQiOjE3MjQ0MDU2MDgsImp0aSI6ImFiNTUyYTI2NWMyZjRiYjliYzRjOGY0ZDE0YWQ5NTEyIiwidXNlcl9pZCI6MX0.xrn7V1NKTs_7UrEALJvhtqWF6Fu9Hp2pwm7SaqiGk5c"
 
-function makeAuthenticatedRequest(url, options = {}) {
+function makeUnauthenticatedRequest(url, options = {}) {
 
 	const csrfToken = getCookie('csrftoken');
 	if (!csrfToken) {
@@ -146,8 +155,8 @@ function makeAuthenticatedRequest(url, options = {}) {
 	}
 
 	options.headers = options.headers || {};
-	options.headers['Authorization'] = `Bearer ${AccessKey}`;
-	options.headers['Refresh'] =
+	options.headers['Accept'] = 'application/json';
+	options.headers['Content-Type'] = 'application/json';
 	options.headers['X-CSRFToken'] = csrfToken;
 	options.credentials = 'include';
 
@@ -162,45 +171,104 @@ function makeAuthenticatedRequest(url, options = {}) {
 				console.log("error 401");
 			}
 			return response;
-		});
+	});
+}
+
+function makeAuthenticatedRequest(url, options = {}) {
+
+	const csrfToken = getCookie('csrftoken');
+	if (!csrfToken) {
+		console.error('CSRF token is missing. Cannot refresh token.');
+		return Promise.reject('CSRF token is missing');
 	}
+
+	options.headers = options.headers || {};
+	options.headers['Authorization'] = `Bearer ${window.accessToken}`;
+	// options.headers['Refresh'] =
+	options.headers['Accept'] = 'application/json';
+	options.headers['Content-Type'] = 'application/json';
+	options.headers['X-CSRFToken'] = csrfToken;
+	options.credentials = 'include';
+
+	return fetch(url, options)
+	.then(response => {
+		if (response.status === 401) {
+			// If token has expired, refresh and retry the request
+			//return refreshToken().then(() => {
+				//	options.headers['Authorization'] = `Bearer ${window.accessToken}`;
+				//	return fetch(url, options);
+				//});
+				console.log("error 401");
+			}
+			return response;
+	});
+}
 
 	// LOGIN
 
-	//function userLogin() {
-	//	email = document.getElementById("emailInput");
-	//	password = document.getElementById("passwordInput");
+function userLogin() {
+	email = document.getElementById("emailInput").value;
+	password = document.getElementById("passwordInput").value;
+	errorMessageContainer = document.getElementById("connectionErrorMessage")
 
-	//	makeAuthenticatedRequest("/api/login/", {
-	//		method: 'POST',
-	//		body:
-	//	})
-	//}
+	console.log(email);
+	console.log(password)
+
+	const data = {
+		email: email,
+		password: password
+	};
+
+	makeUnauthenticatedRequest("/api/login/", {
+		method: 'POST',
+		body: JSON.stringify(data)
+	})
+	.then(response => {
+		if(!response.ok) {
+			throw new Error('Network response was not ok');
+		}
+		return response.json();
+	})
+	.then(data => {
+		if (data.access_token) {
+			window.accessToken = data.access_token;
+			console.log(window.accessToken);
+			console.log('Access token saved');
+		} else {
+			console.error("access token not saved");
+		}
+		ReplaceElement("playerConnection", "buttonsContainer");
+	})
+	.catch(error => {
+		console.error('There was a problem with the fetch operation:', error);
+		errorMessageContainer.innerText = "This user doesn't exist, please create an account"
+	})
+}
 
 	// GET USERS //
 
-	function getUserList() {
-		const userListContainer = document.getElementById('userListContainer');
+function getUserList() {
+	const userListContainer = document.getElementById('userListContainer');
 
-		makeAuthenticatedRequest('/api/users/', {method: 'GET'})
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Network response was not ok');
-            }
-            return response.json(); // Parse the response as JSON
-        })
-        .then(JSONdata => {
-            // Assuming JSONdata is an array of user objects and each user object has a 'username' field
-            const usernames = JSONdata.map(user => user.username); // Extract usernames
-			var responseHTML = "";
-            //userListContainer.innerText = usernames.length; // Display usernames, one per line
-			for (let i = 0; i < usernames.length; i++) {
-				responseHTML = responseHTML + "<div class=\"row m-2\"> <div class=\"h5 col-auto\">" + usernames[i] + "</div> <div class=\"col d-flex justify-content-end\"> <button class=\"btn btn-primary\">Follow</button> </div> </div>"
-			};
-			userListContainer.innerHTML = "<div>" + responseHTML + "</div>";
-		})
-        .catch(error => {
-            console.error('There was a problem with the fetch operation:', error);
-            userListContainer.innerText = 'Error fetching user list';
+	makeAuthenticatedRequest('/api/users/', {method: 'GET'})
+	.then(response => {
+		if (!response.ok) {
+			throw new Error('Network response was not ok');
+		}
+		return response.json(); // Parse the response as JSON
+	})
+	.then(JSONdata => {
+		// Assuming JSONdata is an array of user objects and each user object has a 'username' field
+		const usernames = JSONdata.map(user => user.username); // Extract usernames
+		var responseHTML = "";
+		//userListContainer.innerText = usernames.length; // Display usernames, one per line
+		for (let i = 0; i < usernames.length; i++) {
+			responseHTML = responseHTML + "<div class=\"row m-2\"> <div class=\"h5 col-auto\">" + usernames[i] + "</div> <div class=\"col d-flex justify-content-end\"> <button class=\"btn btn-primary\">Follow</button> </div> </div>"
+		};
+		userListContainer.innerHTML = "<div>" + responseHTML + "</div>";
+	})
+	.catch(error => {
+		console.error('There was a problem with the fetch operation:', error);
+		userListContainer.innerText = 'Error fetching user list';
     });
 }
