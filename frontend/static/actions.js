51a34d8bd732a4572b626a3feccbbfb7a7b534ca
HelloThere-ImMat <dorr.mathias@gmail.com>
2024-08-26 14:55:54 +0200

    adversary connection in course

--- frontend/static/actions.js
+++ frontend/static/actions.js
@@ -2,6 +2,13 @@
 
 let playerIndex = 2;
 let playerNumber = 1;
+let gamesNb = 1;
+let hostConnected = false;
+
+//Tournament of 8 : 7 games
+//Tournament of 6 : 6 games
+//Tournament of 4 : 3 games
+
 
 ///// GENERIC FUNCTION /////
 
@@ -23,13 +30,14 @@ function getCookie(name) {
 
 function ReplaceElement(elementToHideId, elementToShowId)
 {
-	var elementToHide = document.getElementById(elementToHideId);
 
-	elementToHide.classList.add("d-none");
-	elementToHide.classList.remove("d-flex");
+	if (elementToHideId !== "null") {
+		var elementToHide = document.getElementById(elementToHideId);
+		elementToHide.classList.add("d-none");
+		elementToHide.classList.remove("d-flex");
+	}
 
-	if (elementToShowId !== "null")
-	{
+	if (elementToShowId !== "null")	{
 		var elementToShow = document.getElementById(elementToShowId);
 		elementToShow.classList.remove("d-none");
 		elementToShow.classList.add("d-flex");
@@ -46,7 +54,7 @@ document.addEventListener('DOMContentLoaded', function() {
 		if (!response.ok)
 		{
 			ReplaceElement("buttonsContainer", "playerConnection");
-			ReplaceElement("loginBackButton", "null");
+			document.getElementById("loginBackButton").classList.add("d-none");
 		}
 		return response.json()
 	})
@@ -83,9 +91,14 @@ function DisplayTournamentOptions() {
  {
 	playerNumber = playerNb;
 	heading = document.getElementById("loginTitle");
+
+
 	ResetMenuButtons();
 	ReplaceElement("buttonsContainer", "playerConnection");
+	document.getElementById("loginBackButton").classList.remove("d-none");
+	document.getElementById("userForm").reset();
 	heading.innerText = "Player " + playerIndex + " login";
+
  }
 
 function BackButtonConnection()
@@ -214,7 +227,30 @@ function refreshToken()
 			console.log('Access token refreshed');
 	});
 }
-// LOGIN
+// LOGIN & REGISTER
+
+function submitUserForm() {
+	if(connectAccountRadio.checked)
+	{
+		if (!hostConnected) {
+			userLogin();
+		}
+
+		//if adversary connection (1v1)
+			//AddUserToMatch
+		//if adversary connection (tournament)
+			//add user to tournament
+	}
+	else
+	{
+		//if host connection
+		userRegistration();
+
+		//if adversary connection
+			//registercall then addUsertoMatch display connection container again
+		//if
+	}
+}
 
 function userLogin() {
 	email = document.getElementById("emailInput").value;
@@ -222,7 +258,7 @@ function userLogin() {
 	errorMessageContainer = document.getElementById("connectionErrorMessage")
 
 	console.log(email);
-	console.log(password)
+	console.log(password);
 
 	const data = {
 		email: email,
@@ -247,6 +283,49 @@ function userLogin() {
 		} else {
 			console.error("access token not saved");
 		}
+		hostConnected = true;
+		ReplaceElement("playerConnection", "buttonsContainer");
+	})
+	.catch(error => {
+		console.error('There was a problem with the fetch operation:', error);
+		errorMessageContainer.innerText = "This user doesn't exist, please create an account"
+	})
+}
+
+function userRegistration() {
+	email = document.getElementById('emailInput').value;
+	password = document.getElementById('passwordInput').value;
+	passwordComfirmation = document.getElementById('passwordConfirmationInput').value;
+	errorMessageContainer = document.getElementById("connectionErrorMessage")
+
+	const data = {
+		email: email,
+		username: "testUser2",
+		password: password
+	};
+
+	if (password !== passwordComfirmation)
+	{
+		errorMessageContainer.innerText = "The passwords don't match";
+		return;
+	}
+	makeUnauthenticatedRequest("/api/register/", {
+		method: 'POST',
+		body: JSON.stringify(data)
+	}).then(response => {
+		if(!response.ok) {
+			throw new Error('Network response was not ok');
+		}
+		return response.json();
+	}).then(data => {
+		if (data.access) {
+			window.accessToken = data.access;
+			console.log(window.accessToken);
+			console.log('Access token saved');
+		} else {
+			console.error("access token not saved");
+		}
+		hostConnected = true;
 		ReplaceElement("playerConnection", "buttonsContainer");
 	})
 	.catch(error => {
@@ -281,4 +360,42 @@ function getUserList() {
 		console.error('There was a problem with the fetch operation:', error);
 		userListContainer.innerText = 'Error fetching user list';
     });
+}
+
+
+// ADVERSARY CONNECTIONS
+
+function addAdversaryToMatch() {
+
+}
+
+function checkAdversaryCredentials() {
+	email = document.getElementById("emailInput").value;
+	password = document.getElementById("passwordInput").value;
+	errorMessageContainer = document.getElementById("connectionErrorMessage")
+
+	console.log(email);
+	console.log(password);
+
+	const data = {
+		email: email,
+		password: password
+	};
+
+	makeUnauthenticatedRequest("/api/login/", {
+		method: 'POST',
+		body: JSON.stringify(data)
+	})
+	.then(response => {
+		if(!response.ok) {
+			throw new Error('Network response was not ok');
+		}
+		//HERE I NEED TO PASS ON THE USER ID BUT I DONT HAVE IT
+		addAdversaryToMatch();
+		return response.json();
+	})
+	.catch(error => {
+		console.error('There was a problem with the fetch operation:', error);
+		errorMessageContainer.innerText = "This user doesn't exist, please create an account"
+	})	.catch
 }
