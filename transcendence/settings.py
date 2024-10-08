db5c8ce4d9d2b7e2e951d46d35a5a45161b04239
Nassim home <chariinassim@gmail.com>
2024-10-08 19:01:01 +0200

    url media comment

--- transcendence/settings.py
+++ transcendence/settings.py
@@ -196,7 +196,7 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / "frontend/static"]
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
-MEDIA_URL = '/media/'
+#MEDIA_URL = ''
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
