f27f1ba2bbef36b8403a0f27c24defe0f993fc50
Charlelie BESSONNET <cbessonn@paul-f5Br3s4.clusters.42paris.fr>
2024-10-25 14:22:20 +0200

    setup media serving on nginx and change frontend to use the right path for avatars

--- transcendence/settings.py
+++ transcendence/settings.py
@@ -197,7 +197,7 @@ STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, '/nginx/media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
