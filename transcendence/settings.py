45725e53e9e388947f61e907a462f7ab874dffe3
Charlelie BESSONNET <cbessonn@made-f0Ar1s5.clusters.42paris.fr>
2024-10-30 15:00:02 +0100

    fix avatar problem

--- transcendence/settings.py
+++ transcendence/settings.py
@@ -197,7 +197,7 @@ STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, '/nginx/media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
