1a7a58a1a85e33c72a9efb219b74596addb85f0e
Charlelie BESSONNET <cbessonn@paul-f5Br3s5.clusters.42paris.fr>
2024-11-01 16:20:51 +0100

    fix bug

--- transcendence/settings.py
+++ transcendence/settings.py
@@ -196,7 +196,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
-MEDIA_URL = '/usr/media/'
+MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, '/usr/media')
 
 # Default primary key field type
